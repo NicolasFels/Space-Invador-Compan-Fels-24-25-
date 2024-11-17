7aecbc9dbf3dac90f29020221e542c9765e3dc49
Nicolas Fels <nicolas.fels@cpe.fr>
2024-11-17 17:03:36 +0100

    début d'une branche de test

--- Jeu.py
+++ Jeu.py
@@ -4,3 +4,14 @@ NIcolas FELS
 But: Faire la classe Jeu du space invaders
 A faire: tout
 """
+from Visuel import Visuel
+from Objet_spatial import Objet_spatial
+
+class Jeu():
+    def __init__(self,monvisuel):
+        self.creation("test", 5, 100, 100,monvisuel)
+
+    def creation(self,nom, vitesse, x, y,monvisuel):
+        #self.objet = Objet_spatial(nom, vitesse, x, y)
+        monvisuel.canvas.create_rectangle(x, y, x+10, y+10, fill="white")
+        
--- Main.py
+++ Main.py
@@ -7,10 +7,9 @@ A faire:
 import tkinter as tk
 
 from Visuel import Visuel
+from Jeu import Jeu
+from Objet_spatial import Joueur
 
-# from Objet_spatial import Joueur
-
-mf = Visuel()
 # test = Objet_spatial("bob", 5, 100, 100)
 # test.creation(test.x, test.y)
 # test=Joueur("bob,",1,100,100)
@@ -18,3 +17,8 @@ mf = Visuel()
 
 # for key in ["<Left>", "<Right>", "<Up>", "<Down>"]:
 #     Visuel.bind(key, test.deplacement_joueur)
+
+mv = Visuel()
+mg = Jeu(mv)
+
+mv.mf.mainloop()
